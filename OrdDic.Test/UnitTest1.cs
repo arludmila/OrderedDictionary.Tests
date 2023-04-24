88d59f736f77d045bc91617f603e6d8d2fd68b6f
Ludmila <ludmila062001@gmail.com>
2023-04-24 19:32:32 -0300

    Add project files.

---
+++ OrdDic.Test/UnitTest1.cs
@@ -0,0 +1,60 @@
+using System.Collections.Specialized;
+
+namespace OrdDic.Test
+{
+    public class UnitTest1
+    {
+        // TESTS CON ORDERED DICTIONARY
+        OrderedDictionary dic = new OrderedDictionary();
+        [Theory]
+        [InlineData(100)]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        [InlineData(100000)]
+        [InlineData(500000)]
+        public void AgregarElementos(int cantidad)
+        {
+            for (int i = 1; i <= cantidad; i++)
+            {
+                dic.Add(i, $"Alumno {i}");
+            }
+            Assert.Equal(cantidad, dic.Count);
+        }
+        [Theory]
+        [InlineData(100,50)]
+        [InlineData(1000,75)]
+        [InlineData(10000,1000)]
+        [InlineData(100000,5601)]
+        [InlineData(500000,205000)]
+        public void EliminarElementosXKey(int agregar, int eliminar)
+        {
+            for (int i = 1 ; i <= agregar ; i++)
+            {
+                dic.Add(i, $"Alumno {i}");
+            }
+            for (int i = 1 ;i <= eliminar ; i++)
+            {
+                dic.Remove(i);
+            }
+            Assert.Equal(agregar-eliminar, dic.Count);
+        }
+        [Theory]
+        [InlineData(100, 50)]
+        [InlineData(1000, 75)]
+        [InlineData(10000, 1000)]
+        [InlineData(100000, 5601)]
+        [InlineData(500000, 205000)]
+        public void EliminarElementosXIndex(int agregar, int eliminar)
+        {
+            for (int i = 1 ; i <= agregar ; i++)
+            {
+                dic.Add(i, $"Alumno {i}");
+            }
+            for (int i = 1 ; i <= eliminar ; i++)
+            {
+                dic.RemoveAt(i);
+            }
+            Assert.Equal(agregar - eliminar, dic.Count);
+        }
+    }
+}
---
+++ OrdDic.Test/UnitTest2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Specialized;
+
+namespace OrdDic.Test
+{
+    public class UnitTest2
+    {
+        // TESTS CON DICCIONARIO NORMAL
+        Dictionary<int, string> dic = new();
+        [Theory]
+        [InlineData(100)]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        [InlineData(100000)]
+        [InlineData(500000)]
+        public void AgregarElementos(int cantidad)
+        {
+            for (int i = 1 ; i <= cantidad ; i++)
+            {
+                dic.Add(i, $"Alumno {i}");
+            }
+            Assert.Equal(cantidad, dic.Count);
+        }
+        [Theory]
+        [InlineData(100, 50)]
+        [InlineData(1000, 75)]
+        [InlineData(10000, 1000)]
+        [InlineData(100000, 5601)]
+        [InlineData(500000, 205000)]
+        public void EliminarElementos(int agregar, int eliminar)
+        {
+            for (int i = 1 ; i <= agregar ; i++)
+            {
+                dic.Add(i, $"Alumno {i}");
+            }
+            for (int i = 1 ; i <= eliminar ; i++)
+            {
+                dic.Remove(i);
+            }
+            Assert.Equal(agregar - eliminar, dic.Count);
+        }    
+    }
+}
